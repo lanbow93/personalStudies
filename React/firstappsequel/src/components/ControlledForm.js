cba8877d7e963190b00acea0318a0619e47e4a34
lanbow93 <the.doctor1501@gmail.com>
2022-12-20 12:15:05 -0600

    Change state with controlled form using object

--- React/firstappsequel/src/components/ControlledForm.js
+++ React/firstappsequel/src/components/ControlledForm.js
@@ -1,16 +1,25 @@
 import { useState } from "react"
 
 export default function ControlledForm(props){
-    const [todos, setTodos] = useState(["Breakfast", "Lunch"])
+    const [todos, setTodos] = useState([
+        {todo:"Breakfast",
+        other: {cheese: "Gouda"}},
+        {todo:"Lunch",
+        other: {cheese: "Swiss"}}
+    ])
 
     const [form, setForm] = useState({
-        todo: ""
+        todo: "",
+        cheese: ""
     })
 
     function handleSubmit(event){
         event.preventDefault()
         const newState = [...todos]
-        newState.push(form.todo)
+        newState.push({
+            todo: form.todo,
+            other: {cheese: form.cheese}
+        })
         setTodos(newState);
     }
 
@@ -22,9 +31,10 @@ export default function ControlledForm(props){
 
     return <div>
         <form onSubmit={handleSubmit}>
-            <input type="text" name="todo" id="" value={form.todo} onChange={handleChange}/>
+            <input type="text" name="todo" id="" value={form.todo} onChange={handleChange} placeholder="todo"/>
+            <input type="text" name="cheese" id="" value={form.cheese} onChange={handleChange} placeholder="cheese"  />
             <input type="submit" value="Submit" />
         </form>
-        {todos.map((todo, index) => <h1 key={index}>{todo}</h1>)}
+        {todos.map((todo, index) => <h1 key={index}>{todo.todo + " " + todo.other.cheese}</h1>)}
     </div>
 }
